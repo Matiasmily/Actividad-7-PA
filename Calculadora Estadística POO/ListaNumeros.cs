d831d1b79bdbcaca49074596de9b4239745d2908
Mily Matías <158805363+Matiasmily@users.noreply.github.com>
2024-07-28 12:39:42 -0600

    Se realizó la misma calculadora estadísitica ahora con POO

---
+++ Calculadora Estadística POO/ListaNumeros.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Estadística_POO
+{
+    public class ListaNumeros
+    {
+        public List<double> NumbersList {  get; set; }
+
+        public ListaNumeros(List<double> numbersList)
+        {
+            NumbersList = numbersList;
+        }
+
+        public void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("----- CALCULADORA ESTADÍSTICA -----");
+            Console.WriteLine(" -- MENÚ ---");
+            Console.WriteLine("1. Ingresar números");
+            Console.WriteLine("2. Calcular y mostrar la media");
+            Console.WriteLine("3. Calcular y mostrar la mediana");
+            Console.WriteLine("4. Calcular y mostrar la moda");
+            Console.WriteLine("5. Calcular y mostrar la desviación estándar");
+            Console.WriteLine("6. Volver a ingresar números");
+            Console.WriteLine("7. Salir");
+            Console.Write("Ingrese una opción: ");
+        }
+        public void EnterNumbersList()
+        {
+
+            Console.Clear();
+            NumbersList.Clear();
+            Console.WriteLine("--- Ingresar números ---");
+            Console.Write("¿Cuántos números desea ingresar?: ");
+            int size = int.Parse(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Ingrese el No.{i + 1}: ");
+                double number = double.Parse(Console.ReadLine());
+                NumbersList.Add(number);
+
+            }
+            Console.WriteLine("\nHa ingresado los números correctamente");
+            Console.WriteLine("Presione enter para continuar...");
+            Console.ReadKey();
+
+
+        }
+
+        public double CalculateAverage()
+        {
+            double average = 0, addition = 0;
+            foreach (double number in NumbersList)
+            {
+                addition += number;
+            }
+            average = addition / NumbersList.Count();
+            return average;
+        }
+        public double CalculateMedian()
+        {
+            double median = 0;
+            int middlelist = 0;
+            NumbersList = NumbersList.OrderBy(n => n).ToList();
+            if (NumbersList.Count % 2 == 0)
+            {
+                middlelist = NumbersList.Count / 2;
+                median = (NumbersList[middlelist] + NumbersList[middlelist - 1]) / 2;
+
+            }
+            else
+            {
+                middlelist = NumbersList.Count / 2;
+                median = (NumbersList[middlelist]);
+            }
+            return median;
+        }
+        public double CalculateMode()
+        {
+            double mode = NumbersList[0];
+            int maxCount = 0;
+            for (int i = 0; i < NumbersList.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < NumbersList.Count; j++)
+                {
+                    if (NumbersList[i] == NumbersList[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    mode = NumbersList[i];
+                }
+            }
+            return mode;
+        }
+        public double CalculateStandarDeviation()
+        {
+            double mean = CalculateAverage(), addition = 0, variance = 0, standardDeviation = 0;
+            foreach (double number in NumbersList)
+            {
+                addition += Math.Pow(number - mean, 2);
+            }
+            variance = addition / NumbersList.Count;
+            standardDeviation = Math.Sqrt(variance);
+            return standardDeviation;
+        }
+    }
+
+}
---
+++ Calculadora Estadística POO/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Calculadora_Estadística_POO;
+List <double> NumbersList = new List<double>();
+ListaNumeros calculations = new ListaNumeros(NumbersList);
+
+bool continueOne = true;
+do
+{
+    try
+    {
+        Console.Clear();
+        calculations.ShowMenu();
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                calculations.EnterNumbersList();
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("--- Calcular y Mostrar la Media ---");
+                Console.WriteLine($"La media es: {calculations.CalculateAverage()}");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+            case 3:
+                Console.Clear();
+                Console.WriteLine("--- Calcular y Mostrar la Mediana ---");
+                Console.WriteLine($"La mediana es: {calculations.CalculateMedian()}");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+            case 4:
+                Console.Clear();
+                Console.WriteLine("--- Calcular y Mostrar la Moda ---");
+                Console.WriteLine($"La moda es: {calculations.CalculateMode()}");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+            case 5:
+                Console.Clear();
+                Console.WriteLine("--- Calcular y Mostrar la Desviación Estándar ---");
+                Console.WriteLine($"La desviación estándar es: {calculations.CalculateStandarDeviation()}");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+            case 6:
+                Console.Clear();
+                calculations.EnterNumbersList();
+                break;
+            case 7:
+                Console.WriteLine("\nUsted está saliendo del programa...");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                continueOne = false;
+                break;
+            default:
+                Console.WriteLine("\nIngrese una opción válida");
+                Console.WriteLine("Presione Enter para continuar");
+                Console.ReadKey();
+                break;
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Error: Por favor ingrese una opción numérica");
+        Console.WriteLine("Presione Enter para continuar");
+        Console.ReadKey();
+    }
+} while (continueOne);
